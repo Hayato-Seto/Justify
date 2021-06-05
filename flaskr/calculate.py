297ef59721f3e079ae431279cffa4756121eaaff
Hayato Seto <setohayato@ImoBook.local>
2021-06-05 12:21:39 +0900

    change flash message

--- flaskr/calculate.py
+++ flaskr/calculate.py
@@ -12,6 +12,16 @@ def form():
         v2 = int(request.form['value2'])
         v3 = v1+v2
 
-        flash(v3)
+        light_speed_km_s = 30
+        lorentz_factor = (1-(v1/v3)**2)**0.5
+        speed_km_s = round(light_speed_km_s*lorentz_factor, 3)
+        distance_km = round(light_speed_km_s*lorentz_factor*v3*60, 3)
+        # light_year=lorentz_factor*v3/60/24/365 <- use after
+
+        flash(
+            "距離{}万kmの場所から<br>\
+            秒速{}万kmで目的地に向かえば<br>\
+            遅刻は相対論的に正当化されます".format(distance_km, speed_km_s)
+        )
 
     return render_template('calculation.html')
