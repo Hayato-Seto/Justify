791d908af3e11ec64eae82209b7571efc90f0e9f
Hayato Seto <setohayato@ImoBook.local>
2021-06-20 17:31:06 +0900

    change init

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask, render_template
 
 
-def create_app(test_config=None):
+def create_app(self, test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
